916e2d32283f2be81c2837634d3be2bbb0a655d6
Alberto Luvisutto <luvisutto.alberto@gmail.com>
2019-10-20 20:15:46 +0200

    esercizi 03 2

--- ex_adv_prog/03_matrix_transpose.cc
+++ ex_adv_prog/03_matrix_transpose.cc
@@ -3,6 +3,7 @@
 #include <array>
 #include <vector>
 #include <iomanip>
+#include <sstream>
 
 
 template <std::size_t dim>
@@ -30,21 +31,30 @@ std::array<double, dim> transpose(std::array<double, dim>& M, int row, int col){
 	for(int i = 0; i<dim; i++){
 		trasposta[i] = 0;
 	}
-	for(int i = 0; i<col; i++){
-		for(int j=0; j<row; j++){
-			if(trasposta[j*col+i]==0){
-				trasposta[j*col+i] = M[i*col+j];
+	for(int i = 0; i<row; i++){
+		for(int j=0; j<col; j++){
+			if(trasposta[j*row+i]==0){
+				trasposta[j*row+i] = M[i*col+j];
 			}
 		}
 	}
-
 	return trasposta;
 }
 
 
 int main(int argc, char * argv[]){
-	const unsigned int row = 2;
-	const unsigned int col = 3;
+	//come si fa?
+	
+	const unsigned int row = (int) *argv[1];
+	//std::istringstream row_tmp = argv[1];
+	//row_tmp >> row;
+	
+	const unsigned int col =(int) *argv[2];
+	//std::istringstream col_tmp = argv[2];
+	//col_tmp >> col;
+
+	//const unsigned int row = 3;
+	//const unsigned int col = 3;
 	const unsigned int dim = row*col;
 	std::array<double, dim> M;
 	initialize(M, row, col);
---
+++ ex_adv_prog/03_mean_median.cc
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+template <typename T>
+void print_vector(const std::vector<T>& v, const std::string& s) {
+  std::cout << s << ": ";
+  for (const auto& x : v)
+    std::cout << x << " ";
+  std::cout << std::endl;
+}
+
+double mean(std::vector<double> v){
+	double mean;
+	double sum = 0;
+	for(auto i = 0u; i< v.size(); i++){
+		sum += v.at(i);
+	}
+	mean = sum/v.size();
+	return mean;
+}
+
+double median(std::vector<double>& v){
+	double median;
+	std::sort(v.begin(), v.end());
+	print_vector(v, "Il vettore ordinato è ");
+	if(v.size()%2 == 0){
+		int pos1 = v.size()/2;
+		int pos2 = v.size()/2 + 1;
+		std::cout << "Numero pari" << std::endl;
+		std::cout 	<< "Le posizioni sono " << pos1 
+					<< " e " << pos2 << std::endl;
+		std::cout 	<< "I valori sono " << v.at(pos1) << " e "
+					<< v.at(pos2) << std::endl;
+		median = (v.at(pos1)+v.at(pos2))/2;
+	}
+	else{
+		std::cout << "Numero dispari" << std::endl;
+		int pos = (v.size()+1)/2;
+		std::cout << "La posizione è " << pos << std::endl;
+		median = v.at(pos);
+	}
+	return median;
+}
+
+void init(std::vector<double>& v){
+	double x;
+	while((std::cin >> x)){
+		v.push_back(x);
+	}
+}
+
+int main(int argc, char * argv[]){
+	std::vector<double> temperatures;
+	init(temperatures);
+	print_vector(temperatures, "The vector is");
+	double mean_t = mean(temperatures);
+	std::cout << "the mean is " << mean_t << std::endl;
+	double median_t = median(temperatures);
+	std::cout << "the median is " << median_t << std::endl;
+	
+
+
+
+
+
+
+	return 0;
+}
