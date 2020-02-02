5b12ddf22e73695ceb5785875bfe1107ea36f30a
Alberto Luvisutto <luvisutto.alberto@gmail.com>
2020-02-03 00:02:28 +0100

    notes

--- lectures/c++/07_inheritance/07_using.cc
+++ lectures/c++/07_inheritance/07_using.cc
@@ -7,6 +7,7 @@ struct foo {
 struct bar : public foo {
   using foo::f;  // brings all foo::f to this scope
   void f(char x) { std::cout << "char " << x << std::endl; } //without "using" this would be overloading
+  //Non è proprio proprio così, sistema un po' sta cosa
   //overloading is not done with inheritance; without using, the object "33" inserted in the main would be seen as a char, even if it is an int
 };
 
